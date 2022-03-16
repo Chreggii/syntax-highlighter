e1f71528ed34fa70e15a351d1b492597e4f127b2
Oliver Kamer <oliver@kamer.ch>
2022-03-16 16:14:22 +0100

    Add an express Server for the publicAPI

---
+++ publicAPI/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.send("Hi from the public API"));
+
+module.exports = app;
