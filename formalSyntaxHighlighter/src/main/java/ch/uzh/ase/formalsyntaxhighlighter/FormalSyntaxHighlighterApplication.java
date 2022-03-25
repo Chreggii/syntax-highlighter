c0263f648084bde72c04b8f6756c63bb057bd169
Oliver Kamer <oliver@kamer.ch>
2022-03-25 15:45:40 +0100

    highlighting endpoint

--- formalSyntaxHighlighter/src/main/java/ch/uzh/ase/formalsyntaxhighlighter/FormalSyntaxHighlighterApplication.java
+++ formalSyntaxHighlighter/src/main/java/ch/uzh/ase/formalsyntaxhighlighter/FormalSyntaxHighlighterApplication.java
@@ -82,6 +82,69 @@ public class FormalSyntaxHighlighterApplication {
 
     }
 
+    @GetMapping("/highlight-string")
+    public ResponseEntity<Object> highlight(@RequestParam String text, @RequestParam String type) throws NoSuchFieldException {
+        Resolver resolver;
+
+        if (Objects.equals(type, "python")) {
+            resolver = new Python3Resolver();
+        }
+        else if (Objects.equals(type, "kotlin")) {
+            resolver = new KotlinResolver();
+        }
+        else if (Objects.equals(type, "java")) {
+            resolver = new JavaResolver();
+        }
+        else { // Invalid type
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new HashMap<>() {
+                {
+                    put("error", new HashMap<>() {
+                        {
+                            put("code", 400);
+                            put("type", "Bad request");
+                            put("reason", type + " is not a valid type ([python, kotlin, java])");
+                        }
+                    });
+                }
+            });
+        }
+        Object[] hToks = resolver.highlight(text);
+
+        List<Map<String, Integer>> outputData = new ArrayList<>(){
+        };
+        for (Object hTok: hToks){
+            
+            try {
+                // Inflection because we don't have access to hTok through library
+                Class cls = hTok.getClass();
+
+                Field hCodeValueField = cls.getField("hCodeValue");
+                Integer hCodeValue = (Integer) hCodeValueField.get(hTok);
+
+                Field startIndexField = cls.getField("startIndex");
+                Integer startIndex = (Integer) startIndexField.get(hTok);
+
+                Field endIndexField = cls.getField("endIndex");
+                Integer endIndex = (Integer) endIndexField.get(hTok);
+
+                Field tokenIdField = cls.getField("tokenId");
+                Integer tokenId = (Integer) tokenIdField.get(hTok);
+
+                outputData.add(new OrderedHashMap<>(){{
+                    // Ordered hashmap so we get the described output format
+                    put("hCodeValue", hCodeValue);
+                    put("startIndex", startIndex);
+                    put("endIndex", endIndex);
+                    put("tokenId", tokenId);
+                }});
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return ResponseEntity.ok(outputData);
+
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(FormalSyntaxHighlighterApplication.class, args);
     }
