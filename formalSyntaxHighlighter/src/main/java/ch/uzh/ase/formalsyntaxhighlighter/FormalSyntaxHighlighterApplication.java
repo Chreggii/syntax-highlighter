7769fd8c79ed473c800e0cbbb14aa05a0be01cac
Oliver Kamer <oliver@kamer.ch>
2022-03-25 11:27:27 +0100

    Add skeleton for lex-string endpoint

--- formalSyntaxHighlighter/src/main/java/ch/uzh/ase/formalsyntaxhighlighter/FormalSyntaxHighlighterApplication.java
+++ formalSyntaxHighlighter/src/main/java/ch/uzh/ase/formalsyntaxhighlighter/FormalSyntaxHighlighterApplication.java
@@ -2,13 +2,16 @@ package ch.uzh.ase.formalsyntaxhighlighter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import resolver.JavaResolver;
+import resolver.KotlinResolver;
+import resolver.Python3Resolver;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import resolver.*;
+import java.util.*;
 
 @SpringBootApplication
 @RestController
@@ -16,12 +19,58 @@ public class FormalSyntaxHighlighterApplication {
 
     @GetMapping("/")
     public Map<String, Object> main() {
-        return new HashMap<>(){
+        return new HashMap<>() {
             {
                 put("service", "Formal Syntax Highlighter");
                 put("status", "okay");
             }
         };
+    }
+
+    @GetMapping("/lex-string")
+    public ResponseEntity<Object> lex(@RequestParam String text, @RequestParam String type) {
+
+        if (Objects.equals(type, "python")) {
+            Python3Resolver resolver = new Python3Resolver();
+
+            return ResponseEntity.ok(new HashMap<>() {
+                {
+                    put("language", "Python");
+                    put("source code", text);
+                }
+            });
+        }
+        if (Objects.equals(type, "kotlin")) {
+            KotlinResolver resolver = new KotlinResolver();
+
+            return ResponseEntity.ok(new HashMap<>() {
+                {
+                    put("language", "Kotlin");
+                    put("source code", text);
+                }
+            });
+        }
+        if (Objects.equals(type, "java")) {
+            JavaResolver resolver = new JavaResolver();
+            return ResponseEntity.ok(new HashMap<>() {
+                {
+                    put("language", "Java");
+                    put("source code", text);
+                }
+            });
+        }
+        // Invalid type
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new HashMap<>(){
+            {
+                put("error", new HashMap<>(){
+                    {
+                        put("code", 400);
+                        put("type", "Bad request");
+                        put("reason", type + " is not a valid type ([python, kotlin, java])");
+                    }
+                });
+            }
+        });
     }
 
     public static void main(String[] args) {
