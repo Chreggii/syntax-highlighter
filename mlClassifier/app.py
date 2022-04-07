4752ec3b7de7404a1b017f7a47aded390fcea4c9
Oliver Kamer <oliver@kamer.ch>
2022-04-07 23:19:55 +0200

    Formatting through black

--- mlClassifier/app.py
+++ mlClassifier/app.py
@@ -6,41 +6,47 @@ import requests
 app = Flask(__name__)
 
 
-@app.route('/ml-highlight',  methods=['GET'])
+@app.route("/ml-highlight", methods=["GET"])
 def predict(lexing, language):
-    formal_syntax_highligter = requests.get("http://formalSyntaxHighlighter:8080/lex-string", params={'text': lexing, 'type': language})
+    formal_syntax_highligter = requests.get(
+        "http://formalSyntaxHighlighter:8080/lex-string",
+        params={"text": lexing, "type": language},
+    )
     response = formal_syntax_highligter.json()
     data = response[0]
-    tokens= []
+    tokens = []
     for id in data:
-        tokens.append(id['tokenId'])
-    if language == 'python':
-        language = 'python3'
-    model = SHModel(language, 'model_data')
+        tokens.append(id["tokenId"])
+    if language == "python":
+        language = "python3"
+    model = SHModel(language, "model_data")
     model.setup_for_prediction()
     highlighted_data = model.predict(tokens)
 
     return highlighted_data
 
-@app.route('/ml-train',  methods=['PUT'])
+
+@app.route("/ml-train", methods=["PUT"])
 def learn(lexing, language):
-    formal_syntax_highligter = requests.get("http://formalSyntaxHighlighter:8080/highlight-string",
-                                            params={'text': lexing, 'type': language})
+    formal_syntax_highligter = requests.get(
+        "http://formalSyntaxHighlighter:8080/highlight-string",
+        params={"text": lexing, "type": language},
+    )
     response = formal_syntax_highligter.json()
     data_token = response[0]
     tokens = []
     highlighted = []
     for id in data:
-        tokens.append(id['tokenId'])
-        highlighted.append(id['hCodeValue'])
-    if language == 'python':
-        language = 'python3'
-    model = SHModel(language, 'model_data')
+        tokens.append(id["tokenId"])
+        highlighted.append(id["hCodeValue"])
+    if language == "python":
+        language = "python3"
+    model = SHModel(language, "model_data")
     model.setup_for_finetuning()
     # check value later
     value = model.finetune_on(token, highlighted)
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 3000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 3000))
+    app.run(debug=True, host="0.0.0.0", port=port)
