2e6f5111a6af274e96324f9713c6d2f9b15c18e2
nikohlUZH <23641128+nikohlUZH@users.noreply.github.com>
2022-04-09 14:04:18 +0200

    defined way to test flask with parameters

--- mlClassifier/app.py
+++ mlClassifier/app.py
@@ -12,89 +12,90 @@ types = [PYTHON3_LANG_NAME, KOTLIN_LANG_NAME, JAVA_LANG_NAME, "python"]
 # Helper method to make testing easier
 def getLexing(text, type):
     return requests.get(
-        "http://localhost:8080/lex-string",# for docker: http://formalSyntaxHighlighter:8080/lex-string
+        "http://localhost:8080/lex-string",  # for docker: http://formalSyntaxHighlighter:8080/lex-string
         params={"text": text, "type": type},
     )
 
-@app.route("/ml-highlight", methods=["GET"])
-@app.errorhandler(werkzeug.exceptions.BadRequest)
-def predict():
-    # define the parameter that we expect
-    text = request.args.get('text')
-    type = request.args.get('type')
 
-    # check type of 'text' and 'type', return 400 error if is wrong
-    if type not in types:
-        return "The type should be either 'java', 'kotlin', 'python'!", 400
-    if not isinstance(text, str):
-        return "Text should be a string!", 400
+def configure_routes(app):
+    @app.route("/ml-highlight", methods=["GET"])
+    @app.errorhandler(werkzeug.exceptions.BadRequest)
+    def predict():
+        # define the parameter that we expect
+        text = request.args.get('text')
+        type = request.args.get('type')
 
-    # convert 'python3' variable to python for the formalSyntaxHighlighter API call
-    if type == PYTHON3_LANG_NAME:
-        type = 'python'
-    formal_syntax_highlighter = getLexing(text, type)
-    fsh_response = formal_syntax_highlighter.json()
+        # check type of 'text' and 'type', return 400 error if is wrong
+        if type not in types:
+            return "The type should be either 'java', 'kotlin', 'python'!", 400
+        if not isinstance(text, str):
+            return "Text should be a string!", 400
 
-    # collect tokenId from formalSyntaxHighlighter API response
-    tokens = []
-    for ltok in fsh_response:
-        tokens.append(ltok["tokenId"])
+        # convert 'python3' variable to python for the formalSyntaxHighlighter API call
+        if type == PYTHON3_LANG_NAME:
+            type = 'python'
+        formal_syntax_highlighter = getLexing(text, type)
+        fsh_response = formal_syntax_highlighter.json()
 
-    # convert type to 'python3' for the SHModel
-    if type == 'python':
-        type = PYTHON3_LANG_NAME
-    model = SHModel(
-        type, "model_data"
-    )
-    model.setup_for_prediction()
-    highlighted_data = model.predict(tokens)
+        # collect tokenId from formalSyntaxHighlighter API response
+        tokens = []
+        for ltok in fsh_response:
+            tokens.append(ltok["tokenId"])
 
-    response_data = {
-        'lexingData': fsh_response,
-        'hCodeValues': highlighted_data
-    }
+        # convert type to 'python3' for the SHModel
+        if type == 'python':
+            type = PYTHON3_LANG_NAME
+        model = SHModel(
+            type, "model_data"
+        )
+        model.setup_for_prediction()
+        highlighted_data = model.predict(tokens)
 
-    return response_data
+        response_data = {
+            'lexingData': fsh_response,
+            'hCodeValues': highlighted_data
+        }
 
+        return response_data
 
-@app.route("/ml-train", methods=["PUT"])
-def learn():
-    text = request.args.get('text')
-    type = request.args.get('type')
+    @app.route("/ml-train", methods=["PUT"])
+    def learn():
+        text = request.args.get('text')
+        type = request.args.get('type')
 
-    if type not in types:
-        return "The type should be either 'java', 'kotlin', 'python'!", 400
+        if type not in types:
+            return "The type should be either 'java', 'kotlin', 'python'!", 400
 
-    if not isinstance(text, str):
-        return "Text should be a string!", 400
+        if not isinstance(text, str):
+            return "Text should be a string!", 400
 
-    if type == PYTHON3_LANG_NAME:
-        type = 'python'
+        if type == PYTHON3_LANG_NAME:
+            type = 'python'
 
-    formal_syntax_highlighter = requests.get(
-        "http://localhost:8080/highlight-string",# for docker: http://formalSyntaxHighlighter:8080/highlight-string
-        params={"text": text, "type": type},
-    )
-    fsh_response = formal_syntax_highlighter.json()
+        formal_syntax_highlighter = requests.get(
+            "http://localhost:8080/highlight-string",
+            # for docker: http://formalSyntaxHighlighter:8080/highlight-string
+            params={"text": text, "type": type},
+        )
+        fsh_response = formal_syntax_highlighter.json()
 
-    tokens = []
-    highlighted = []
-    for id in fsh_response:
-        tokens.append(id["tokenId"])
-        highlighted.append(id["hCodeValue"])
+        tokens = []
+        highlighted = []
+        for id in fsh_response:
+            tokens.append(id["tokenId"])
+            highlighted.append(id["hCodeValue"])
 
-    if type == "python":
-        type = PYTHON3_LANG_NAME
+        if type == "python":
+            type = PYTHON3_LANG_NAME
 
-    model = SHModel(type, "model_data")
-    model.setup_for_finetuning()
+        model = SHModel(type, "model_data")
+        model.setup_for_finetuning()
 
-    finetuning = model.finetune_on(tokens, highlighted)
-    model.persist_model()
+        finetuning = model.finetune_on(tokens, highlighted)
+        model.persist_model()
 
-    return "Training loss: {}".format(finetuning)
+        return "Training loss: {}".format(finetuning)
 
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    if __name__ == "__main__":
+        port = int(os.environ.get("PORT", 3000))
+        app.run(debug=True, host="0.0.0.0", port=port)
--- mlClassifier/test_app.py
+++ mlClassifier/test_app.py
@@ -1,15 +1,38 @@
+import json
+
+import pytest
+import requests
+import requests_mock
+
+from flask import Flask, Response
+
 import app
 import mock
 import unittest
 
+from app import configure_routes
+
 class FlaskTest(unittest.TestCase):
 
-    # Mocking the helper method
-    app.getLexing = mock.Mock(return_value="mocked stuff")
+    # Mocking the helper method - Old Try
+    #app.getLexing = mock.Mock(return_value=['name', 'name2'])
 
-    def test_predict(self):
-        return True
+    # Mocks a Json Response
+    data = {"name":"John", "age":30, "city":"New York"}
+    data = json.dumps(data)
+    response = app.app.response_class(response=data, mimetype='application/json')
+    app.getLexing = mock.Mock(return_value=response)
 
-
+    # Todo Currently we get an internal server error -> For some reason the fsh_response = formal_syntax_highlighter.json() is unable to get the correct json
+    def test_predict(client):
+        app = Flask(__name__)
+        configure_routes(app)
+        client = app.test_client()
+        url = "/ml-highlight/"
+        response = client.get(
+            '/ml-highlight',
+            query_string=dict(text='print("Hello World)', type='python')
+        )
+        assert response.status_code == 200
 
 
