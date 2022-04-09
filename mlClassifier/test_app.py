88ad9f5bbdb27a3f8154f018483c6756a4a8aeee
nikohlUZH <23641128+nikohlUZH@users.noreply.github.com>
2022-04-09 16:07:01 +0200

    Statuss 200 test for ml train

--- mlClassifier/test_app.py
+++ mlClassifier/test_app.py
@@ -1,21 +1,24 @@
-import json
 from flask import Flask, jsonify
-
 import app
 import mock
 import unittest
-
 from app import configure_routes
 
 class FlaskTest(unittest.TestCase):
 
     # Mocking the get Lexing Function
     with app.app.app_context():
-        data = [{"startIndex":0,"endIndex":4,"tokenId":42},{"startIndex":5,"endIndex":7,"tokenId":3}]
+        data = [{"startIndex": 0, "endIndex": 4, "tokenId": 42}, {"startIndex": 5, "endIndex": 7, "tokenId": 3}]
         response = jsonify(data)
         app.getLexing = mock.Mock(return_value=jsonify(data))
 
-    #Basic Test
+    # Mocking the get Highlighting Function
+    with app.app.app_context():
+        data = [{"hCodeValue": 9, "startIndex": 0, "endIndex": 4, "tokenId": 42}, {"hCodeValue": 0, "startIndex": 5, "endIndex": 5, "tokenId": 54}]
+        response = jsonify(data)
+        app.getHighlightString = mock.Mock(return_value=jsonify(data))
+
+    #Basic Test for predicting
     def test_predict(client):
         app = Flask(__name__)
         configure_routes(app)
@@ -25,5 +28,17 @@ class FlaskTest(unittest.TestCase):
             query_string=dict(text='print', type='python')
         )
         assert response.status_code == 200
+
+    # Basic Test for training
+    def test_learn(client):
+        app = Flask(__name__)
+        configure_routes(app)
+        client = app.test_client()
+        response = client.put(
+            '/ml-train',
+            query_string=dict(text='print', type='python')
+        )
+        assert response.status_code == 200
+
 
 
