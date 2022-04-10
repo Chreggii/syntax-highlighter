02f96f3ba0d873c8384072843cc0b1748e66cf43
Oliver Kamer <oliver@kamer.ch>
2022-04-10 14:06:18 +0200

    black cleanup

--- mlClassifier/app.py
+++ mlClassifier/app.py
@@ -31,8 +31,8 @@ def create_app():
     @app.errorhandler(werkzeug.exceptions.BadRequest)
     def predict():
         # define the parameter that we expect
-        text = request.args.get('text')
-        type = request.args.get('type')
+        text = request.args.get("text")
+        type = request.args.get("type")
 
         # check type of 'text' and 'type', return 400 error if is wrong
         if type not in types:
@@ -42,7 +42,7 @@ def create_app():
 
         # convert 'python3' variable to python for the formalSyntaxHighlighter API call
         if type == PYTHON3_LANG_NAME:
-            type = 'python'
+            type = "python"
         formal_syntax_highlighter = getLexing(text, type)
         fsh_response = formal_syntax_highlighter.json()
 
@@ -52,25 +52,20 @@ def create_app():
             tokens.append(ltok["tokenId"])
 
         # convert type to 'python3' for the SHModel
-        if type == 'python':
+        if type == "python":
             type = PYTHON3_LANG_NAME
-        model = SHModel(
-            type, "model_data"
-        )
+        model = SHModel(type, "model_data")
         model.setup_for_prediction()
         highlighted_data = model.predict(tokens)
 
-        response_data = {
-            'lexingData': fsh_response,
-            'hCodeValues': highlighted_data
-        }
+        response_data = {"lexingData": fsh_response, "hCodeValues": highlighted_data}
 
         return response_data
 
     @app.route("/ml-train", methods=["PUT"])
     def learn():
-        text = request.args.get('text')
-        type = request.args.get('type')
+        text = request.args.get("text")
+        type = request.args.get("type")
 
         if type not in types:
             return "The type should be either 'java', 'kotlin', 'python'!", 400
@@ -79,7 +74,7 @@ def create_app():
             return "Text should be a string!", 400
 
         if type == PYTHON3_LANG_NAME:
-            type = 'python'
+            type = "python"
 
         formal_syntax_highlighter = getHighlightString(text, type)
         fsh_response = formal_syntax_highlighter.json()
