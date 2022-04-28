a2931772bcb29a5cd897b5cbfc976f6e961c3b4e
nikohlUZH <23641128+nikohlUZH@users.noreply.github.com>
2022-04-28 13:59:46 +0200

    improved code style

--- mlClassifier/test_app.py
+++ mlClassifier/test_app.py
@@ -1,5 +1,5 @@
 """
-
+Tests for the mlClassifier
 """
 import pytest
 import mock
@@ -14,64 +14,66 @@ with create_app().app_context():
         {"startIndex": 0, "endIndex": 4, "tokenId": 42},
         {"startIndex": 5, "endIndex": 7, "tokenId": 3},
     ]
-    response = requests.Response()
-    response.code = "expired"
-    response.error_type = "expired"
-    response.status_code = 200
-    response._content = (
+    RESPONSE = requests.Response()
+    RESPONSE.code = "expired"
+    RESPONSE.error_type = "expired"
+    RESPONSE.status_code = 200
+    RESPONSE._content = (
         b'[{ "startIndex": 0, "endIndex": 4, "tokenId": 42}, {"startIndex": 5, "endIndex": 7, '
         b'"tokenId": 3}] '
     )
-    app.get_lexing = mock.Mock(return_value=response)
+    app.get_lexing = mock.Mock(return_value=RESPONSE)
 
 # Mocking the get Highlighting Function
 with create_app().app_context():
-    response = requests.Response()
-    response.code = "expired"
-    response.error_type = "expired"
-    response._content = (
+    RESPONSE = requests.Response()
+    RESPONSE.code = "expired"
+    RESPONSE.error_type = "expired"
+    RESPONSE._content = (
         b'[{"hCodeValue": 9, "startIndex": 0, "endIndex": 4, "tokenId": 42}, {"hCodeValue": 0, '
         b'"startIndex": 5, "endIndex": 5, "tokenId": 54}] '
     )
-    app.get_highlight_string = mock.Mock(return_value=response)
+    app.get_highlight_string = mock.Mock(return_value=RESPONSE)
 
 
 @pytest.fixture()
-def app():
-    app = create_app()
-    yield app
+def get_app():
+    """gets the app"""
+    created_app = create_app()
+    yield created_app
 
 
 @pytest.fixture()
-def client(app):
-    return app.test_client()
+def test_client(get_app):
+    """gets the test client"""
+    return get_app.test_client()
 
 
-# Basic Test for predicting
-def test_predict_200(client):
-    response_200 = client.get(
+def test_predict_200(test_client):
+    """Basic Test for predicting"""
+    response_200 = test_client.get(
         "/ml-highlight", query_string=dict(text="print", type="python")
     )
     assert response_200.status_code == 200
 
 
-# Should throw error if invalid language is chosen
-def test_predict_bad_request(client):
-    response_400 = client.get(
+def test_predict_bad_request(test_client):
+    """Should throw error if invalid language is chosen"""
+    response_400 = test_client.get(
         "/ml-highlight", query_string=dict(text="print", type="invalid_language")
     )
     assert response_400.status_code == 400
 
 
-# Basic Test for training
-def test_learn_200(client):
-    response_200 = client.put("/ml-train", json=dict(text="print", type="python"))
+def test_learn_200(test_client):
+    """Basic Test for training"""
+    response_200 = test_client.put("/ml-train", json=dict(text="print", type="python"))
     assert response_200.status_code == 200
 
 
-# Should throw error if invalid language is chosen
-def test_learn_bad_request(client):
-    response_400 = client.put(
+def test_learn_bad_request(test_client):
+    """Should throw error if invalid language is chosen"""
+    response_400 = test_client.put(
         "/ml-train", json=dict(text="print", type="invalid_language")
     )
     assert response_400.status_code == 400
