f0a51f27772d5b046a78538085063f7253d6e5e6
Oliver Kamer <oliver@kamer.ch>
2022-05-01 20:25:40 +0200

    Correct some stuff from pylint

--- mlClassifier/test_app.py
+++ mlClassifier/test_app.py
@@ -1,8 +1,12 @@
+"""
+
+"""
 import pytest
 import mock
+import requests
+
 import app
 from app import create_app
-import requests
 
 # Mocking the get Lexing Function
 with create_app().app_context():
@@ -18,7 +22,7 @@ with create_app().app_context():
         b'[{ "startIndex": 0, "endIndex": 4, "tokenId": 42}, {"startIndex": 5, "endIndex": 7, '
         b'"tokenId": 3}] '
     )
-    app.getLexing = mock.Mock(return_value=response)
+    app.get_lexing = mock.Mock(return_value=response)
 
 # Mocking the get Highlighting Function
 with create_app().app_context():
@@ -29,7 +33,7 @@ with create_app().app_context():
         b'[{"hCodeValue": 9, "startIndex": 0, "endIndex": 4, "tokenId": 42}, {"hCodeValue": 0, '
         b'"startIndex": 5, "endIndex": 5, "tokenId": 54}] '
     )
-    app.getHighlightString = mock.Mock(return_value=response)
+    app.get_highlight_string = mock.Mock(return_value=response)
 
 
 @pytest.fixture()
@@ -45,27 +49,27 @@ def client(app):
 
 # Basic Test for predicting
 def test_predict_200(client):
-    response = client.get(
+    response_200 = client.get(
         "/ml-highlight", query_string=dict(text="print", type="python")
     )
-    assert response.status_code == 200
+    assert response_200.status_code == 200
 
 
 # Should throw error if invalid language is chosen
 def test_predict_bad_request(client):
-    response = client.get(
+    response_400 = client.get(
         "/ml-highlight", query_string=dict(text="print", type="invalid_language")
     )
-    assert response.status_code == 400
+    assert response_400.status_code == 400
 
 
 # Basic Test for training
 def test_learn_200(client):
-    response = client.put("/ml-train", json=dict(text="print", type="python"))
-    assert response.status_code == 200
+    response_200 = client.put("/ml-train", json=dict(text="print", type="python"))
+    assert response_200.status_code == 200
 
 
 # Should throw error if invalid language is chosen
 def test_learn_bad_request(client):
-    response = client.put("/ml-train", json=dict(text="print", type="invalid_language"))
-    assert response.status_code == 400
+    response_400 = client.put("/ml-train", json=dict(text="print", type="invalid_language"))
+    assert response_400.status_code == 400
