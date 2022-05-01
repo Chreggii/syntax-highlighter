65e32095ca85f0cee0e2f5a55d4f28ca0f9dd84c
Christian Omlin <christian.omlin@siemens.com>
2022-05-01 15:46:53 +0200

    style: introduce linter & prettier for ts files

--- publicAPI/src/hcode-values/hcode-values.controller.ts
+++ publicAPI/src/hcode-values/hcode-values.controller.ts
@@ -1,64 +1,51 @@
-import {
-    Controller,
-    Get,
-    HttpException,
-    HttpService,
-    HttpStatus,
-    Param,
-  } from "@nestjs/common";
+import { Controller, Get, HttpException, HttpService, HttpStatus, Param } from '@nestjs/common';
+import { of } from 'rxjs';
+import { HCodeValue } from 'src/models/hCodeValue.type';
+import { HCodeValues } from 'src/models/hCodeValues.model';
 
-  import { HCodeValue } from "src/models/hCodeValue.type";
-  import { HCodeValues } from "src/models/hCodeValues.model";
+@Controller("h-code-value")
+export class HCodeValuesController {
+  constructor(private httpService: HttpService) { }
 
-  import { of } from "rxjs";
-  
-  @Controller("h-code-value")
-  export class HCodeValuesController {
-    constructor(
-      private httpService: HttpService
-    ) {}
-  
-    @Get()
-    async getAllHCodeValues(): Promise<any>{
-        const hCodeValues = (
-            await this.httpService
-              .get(`http://formalSyntaxHighlighter:8080/highlighting-codes`)
-              .toPromise()
-          ).data as HCodeValues[];
+  @Get()
+  async getAllHCodeValues(): Promise<any> {
+    const hCodeValues = (
+      await this.httpService
+        .get(`http://formalSyntaxHighlighter:8080/highlighting-codes`)
+        .toPromise()
+    ).data as HCodeValues[];
 
+    return of(hCodeValues).toPromise();
+  }
 
-        return of(hCodeValues).toPromise();
+  @Get(":value")
+  async getHCodeValue(@Param("value") value: HCodeValue): Promise<any> {
+    const hCodeValues = (
+      await this.httpService
+        .get(`http://formalSyntaxHighlighter:8080/highlighting-codes`)
+        .toPromise()
+    ).data as HCodeValues[];
+
+    let hCodeName = "";
+    for (let i = 0; i < hCodeValues.length; i++) {
+      if (hCodeValues[i]["hCodeValue"] === Number(value)) {
+        hCodeName = hCodeValues[i]["name"];
+      }
     }
-  
-    @Get(':value')
-    async getHCodeValue(@Param("value") value: HCodeValue): Promise<any>{
-        const hCodeValues = (
-            await this.httpService
-              .get(`http://formalSyntaxHighlighter:8080/highlighting-codes`)
-              .toPromise()
-          ).data as HCodeValues[];
 
-        let hCodeName = "";
-        for(var i = 0; i<hCodeValues.length; i++){
-            if (hCodeValues[i]["hCodeValue"] === Number(value)) {
-                hCodeName = hCodeValues[i]["name"];
-            }
-        }
-
-        if (hCodeName){
-            return of({
-                "name": hCodeName, 
-                "hCodeValue": value, "color": "color"
-            }).toPromise();
-        } else {
-            throw new HttpException(
-                {
-                  error:
-                    "HCode for " + value + " does not exist.",
-                },
-                HttpStatus.NOT_FOUND
-              );
-        }
+    if (hCodeName) {
+      return of({
+        name: hCodeName,
+        hCodeValue: value,
+        color: "color",
+      }).toPromise();
+    } else {
+      throw new HttpException(
+        {
+          error: "HCode for " + value + " does not exist.",
+        },
+        HttpStatus.NOT_FOUND
+      );
     }
-            
+  }
 }
--- publicAPI/src/models/hCodeValues.model.ts
+++ publicAPI/src/models/hCodeValues.model.ts
@@ -1,5 +1,4 @@
 export interface HCodeValues {
-    name: string;
-    hCodeValue: number;
-  }
-  
+  name: string;
+  hCodeValue: number;
+}
