cbf1d92c09182c06d595211fb3d707f4a5158953
Oliver Kamer <oliver@kamer.ch>
2022-05-15 20:30:46 +0200

    apply new rules

--- formalSyntaxHighlighter/src/test/java/ch/uzh/ase/formalsyntaxhighlighter/HttpRequestTestHighlighting.java
+++ formalSyntaxHighlighter/src/test/java/ch/uzh/ase/formalsyntaxhighlighter/HttpRequestTestHighlighting.java
@@ -1,7 +1,5 @@
 package ch.uzh.ase.formalsyntaxhighlighter;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
--- formalSyntaxHighlighter/src/test/java/ch/uzh/ase/formalsyntaxhighlighter/HttpRequestTestLexing.java
+++ formalSyntaxHighlighter/src/test/java/ch/uzh/ase/formalsyntaxhighlighter/HttpRequestTestLexing.java
@@ -1,7 +1,5 @@
 package ch.uzh.ase.formalsyntaxhighlighter;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.*;
 import org.springframework.beans.factory.annotation.Autowired;
