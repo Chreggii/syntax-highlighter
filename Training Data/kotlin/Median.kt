189a265ee3993fa9590001869fb5e637746e5f29
Oliver Kamer <oliver@kamer.ch>
2022-05-26 11:35:05 +0200

    add more kotlin data

--- Training Data/kotlin/Median.kt
+++ Training Data/kotlin/Median.kt
@@ -1,39 +1,24 @@
 package math
 
-import java.util.Arrays.sort
+import math.median
+import org.junit.Test
 
-/**
- * Calculates the median of an array of Int
- *
- * @param values is an array of Int
- * @return the middle number of the array
- */
-fun median(values: IntArray): Double {
-    sort(values)
-    return when {
-        values.size % 2 == 0 -> getHalfwayBetweenMiddleValues(values)
-        else -> getMiddleValue(values)
+class Median {
+    @Test
+    fun testMedianWithEvenSize() {
+        val array = intArrayOf(1, 2, 3, 4)
+        assert(median(array) == 2.5)
     }
-}
 
-/**
- * Calculates the middle number of an array when the size is an even number
- *
- * @param values is an array of Int
- * @return the middle number of the array
- */
-private fun getHalfwayBetweenMiddleValues(values: IntArray): Double {
-    val arraySize = values.size
-    val sumOfMiddleValues = (values[arraySize / 2] + values[(arraySize / 2) - 1 ])
-    return sumOfMiddleValues / 2.0
-}
+    @Test
+    fun testMedianWithOddSize() {
+        val array = intArrayOf(1, 2, 3)
+        assert(median(array) == 2.0)
+    }
 
-/**
- * Calculates the middle number of an array when the size is an odd number
- *
- * @param values is an array of Int
- * @return the middle number of the array
- */
-private fun getMiddleValue(values: IntArray): Double {
-    return values[values.size / 2].toDouble()
+    @Test
+    fun testMedianWithNegatives() {
+        val array = intArrayOf( 2, 3, 4, -2, -8, -3)
+        assert(median(array) == 0.0)
+    }
 }
