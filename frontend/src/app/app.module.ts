97728050ee564e67cca5fb355ed570a9d4a3b728
nikohlUZH <23641128+nikohlUZH@users.noreply.github.com>
2022-05-28 17:13:25 +0200

    removed hcode handler component since it is no longer needed

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { AppComponent } from './app.component';
 import { CodePresenterComponent } from './components/code-presenter/code-presenter.component';
 import { SafePipe } from './pipes/safe.pipe';
 import { CodeUploaderComponent } from './components/code-uploader/code-uploader.component';
-import { HCodeValuesHandlerComponent } from './components/h-code-values-handler/h-code-values-handler.component';
 import { TopNavigationBarComponent } from './components/top-navigation-bar/top-navigation-bar.component';
 import { PageHomeComponent } from './components/page-home/page-home.component';
 import { PageFormalModelComponent } from './components/page-formal-model/page-formal-model.component';
@@ -30,7 +29,6 @@ import {MatIconModule} from "@angular/material/icon";
     CodePresenterComponent,
     SafePipe,
     CodeUploaderComponent,
-    HCodeValuesHandlerComponent,
     TopNavigationBarComponent,
     PageHomeComponent,
     PageFormalModelComponent,
