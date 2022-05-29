4f9cce002b7b446bdbff8d9613c5bc94d2a313a6
Oliver Kamer <oliver@kamer.ch>
2022-05-29 21:27:48 +0200

    script for file training

--- Training Data/train_from_files.py
+++ Training Data/train_from_files.py
@@ -1,10 +1,24 @@
 import glob
+import time
+
 import requests
 from tqdm import tqdm
 
-train_files = [glob.glob(f"train_data/**/*.{e}") for e in ["java", "kt", "py"]]
+train_files = [
+    item for sublist in [
+        glob.glob(f"train_data/**/*.{e}") for e in ["java", "kt", "py"]
+    ] for item in sublist
+]
+
+failed_files = []
 
 for file in tqdm(train_files):
-    print(f"Requesting: {file}")
-    files = {"file": open("input_data/java/AES.java", "rb")}
-    requests.post("http://localhost:3000/highlight-file", files=files)
+    files = {"file": open(file, "rb")}
+    try:
+        r = requests.post("http://localhost:3000/highlight-file", files=files)
+        r.raise_for_status()
+    except:
+        time.sleep(10)  # Sleep to allow restart
+        failed_files.append(file)
+
+print(failed_files)
