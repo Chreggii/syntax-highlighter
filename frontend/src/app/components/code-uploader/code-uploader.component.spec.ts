39bbf1c8882b354f1ada8be0c93aac6e5438e075
nikohlUZH <23641128+nikohlUZH@users.noreply.github.com>
2022-05-29 16:39:33 +0200

    even more tests

--- frontend/src/app/components/code-uploader/code-uploader.component.spec.ts
+++ frontend/src/app/components/code-uploader/code-uploader.component.spec.ts
@@ -9,6 +9,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from '../../app.component';
 import { getBaseUrl } from '../../functions/url-resolver.function';
+import {HighlightService} from "../../services/highlighter/highlight.service";
 
 describe('CodeUploaderComponent', () => {
   let httpMock: HttpTestingController;
@@ -33,7 +34,7 @@ describe('CodeUploaderComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should send request with empty fields', () => {
+  it('should send text request with empty fields', () => {
     const fixture = TestBed.createComponent(CodeUploaderComponent);
     const app = fixture.componentInstance;
     app.sendTextRequest();
@@ -41,11 +42,35 @@ describe('CodeUploaderComponent', () => {
     expect(req.request.method).toBe('POST');
   });
 
-  it('should not send request when no file found', () => {
+  it('should not send file request when no file found', () => {
     const fixture = TestBed.createComponent(CodeUploaderComponent);
     const app = fixture.componentInstance;
     const file = app.uploadFile({ target: { files: [] } });
     app.sendFileRequest(file, 'darcula');
     httpMock.expectNone(`${getBaseUrl()}/highlight-file`);
+  });
+
+  it('should send Text request without html', () => {
+    const fixture = TestBed.createComponent(CodeUploaderComponent);
+    const app = fixture.componentInstance;
+    app.formText.get('returnHtml')?.setValue('no');
+    app.formText.get('sourceText')?.setValue('print(\'hello\')');
+    app.formText.get('language')?.setValue('python');
+    app.formText.get('mode')?.setValue('classic');
+    app.sendTextRequest();
+    const req = httpMock.expectOne(`${getBaseUrl()}/highlight-text`);
+    expect(req.request.method).toBe('POST');
+  });
+
+  it('should send Text request with html', () => {
+    const fixture = TestBed.createComponent(CodeUploaderComponent);
+    const app = fixture.componentInstance;
+    app.formText.get('returnHtml')?.setValue('yes');
+    app.formText.get('sourceText')?.setValue('print(\'hello\')');
+    app.formText.get('language')?.setValue('python');
+    app.formText.get('mode')?.setValue('classic');
+    app.sendTextRequest();
+    const req = httpMock.expectOne(`${getBaseUrl()}/highlight-text-html`);
+    expect(req.request.method).toBe('POST');
   });
 });
